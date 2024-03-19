683f67c7f3d49bc6b652a40bbbc6c86cf09f0712
Simon Chan <1330321+yume-chan@users.noreply.github.com>
2024-03-19 21:05:31 +0800

    add counter

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,7 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
+import Counter from "./components/Counter";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
           <Title>SolidStart - Basic</Title>
           <a href="/">Index</a>
           <a href="/about">About</a>
+          <Counter/>
           <Suspense>{props.children}</Suspense>
         </MetaProvider>
       )}
